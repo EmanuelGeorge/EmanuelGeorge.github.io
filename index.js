7018b75f4c784322626abcda3a479a2c9031fde2
EmanuelGeorge <predator69tfw@gmail.com>
2021-01-12 20:05:27 +0200

    .

--- index.js
+++ index.js
@@ -66,7 +66,9 @@ var imageUrls   = ['url("images/1.jpg")','url("images/2.jpg")', 'url("images/3.j
 					 'url("images/257.jpg")', 'url("images/258.jpg")', 'url("images/259.jpg")', 'url("images/260.jpg")',
 					 'url("images/261.jpg")','url("images/262.jpg")', 'url("images/263.jpg")', 'url("images/264.jpg")',
                      'url("images/265.jpg")', 'url("images/266.jpg")', 'url("images/267.jpg")', 'url("images/268.jpg")',
-                     'url("images/269.jpg")','url("images/270.jpg")']
+                     'url("images/269.jpg")','url("images/270.jpg")','url("images/271.jpg")','url("images/272.jpg")',
+					 'url("images/273.jpg")','url("images/274.jpg")','url("images/275.jpg")','url("images/276.jpg")',
+					 'url("images/277.jpg")','url("images/278.jpg")']
 
 
 
